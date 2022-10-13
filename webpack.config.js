31f44698ec12936e93fa6a75df4ec2eeaab71010
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-13 15:36:28 -0500

    before deploy

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports={
         new HtmlWebpackPlugin({
             inject:true,
             template:"./public/index.html",
-            filename:"./public/index.html" //?
+            filename:"index.html" 
         })
     ]
 }

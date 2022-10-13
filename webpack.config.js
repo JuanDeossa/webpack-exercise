3ce36b1b3ca528c888d94c1ee36ac7fd5957799f
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-13 12:12:40 -0500

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports={
+    entry:"./src/index.js", //punto de entrada
+    output:{
+        path:path.resolve(__dirname,"dist"), // Dist es nombre estandar para especificar carpeta de distribucion.
+        filename:"main.js" // Nombre opcional
+    },
+    resolve:{ //Permite identificar las diferentes extensiones con la sque vamos a trabajar
+        extensions:[".js"]
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js?$/, //identifica archivos js
+                exclude:/node_modules/, //excluye carpeta de modulos de node
+                use:{
+                    loader:"babel-loader"
+                }
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            inject:true,
+            template:"./public/index.html",
+            filename:"./public/index.html" //?
+        })
+    ]
+}
